e4f78909fa1202935b87f3d2cb7bd219a67f5a39
RobertPeng <jenpopeng@gmail.com>
2021-08-07 10:15:24 -0400

    resume to work on this project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Navbar from "./Navbar";
-import Home from "./Home";
-import AboutMe from "./AboutMe";
-import Projects from "./Projects";
-import Contact from "./Contact";
+import Navbar from "./components/UI/Navbar";
+import Home from "./components/Home";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 //Material-UI
 import { ThemeProvider } from "@material-ui/styles";
-import theme from "./Theme";
-import Footer from "./Footer";
+import theme from "./components/UI/Theme";
+import Footer from "./components/UI/Footer";
 
 function App() {
   return (
