03207279cc2f2e488ace012bd227649314bdf0e4
RobertPeng <jenpopeng@gmail.com>
2021-09-06 10:20:47 -0400

    add 2019 project text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,32 +5,43 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { Box } from "@material-ui/core";
 import Link from '@material-ui/core/Link';
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   containerAll: {
     width: "100%",
+    marginBottom: "1rem",
   },
   containerYear: {
     //border: "solid",
   },
+  projectContainer: {
+    height: "20rem",
+    width: "20rem",
+  },
   imgBox: {
-    //backgroundColor: theme.palette.primary.main,
     height: "12rem",
     width: "18rem",
     //border: "solid red",
   },
   imgBoxOkinawa: {
+    margin: "auto",
+    marginTop: "1rem",
     height: "12rem",
     width: "18rem",
     //border: "solid red",
-    border: `${theme.palette.primary.main} solid 0.1rem`,
+    border: `${theme.palette.primary.main} solid 0.2rem`,
     backgroundImage: `url(okinawaImg.png)`,
     backgroundSize: "cover",
   },
   text: {
-    backgroundColor: theme.palette.secondary.main,
+    margin: "auto",
+    marginTop: "0.5rem",
+    fontSize: "1.2rem",
+    textAlign: "center",
     height: "6rem",
     width: "18rem",
+    //border: "solid red",
   },
   title: {
     marginLeft: "1rem",
@@ -96,15 +107,17 @@ const Projects = () => {
           <Grid item>
             <Grid container justify="space-evenly">
               <Grid item>
+                <Paper className={classes.projectContainer}>
                 <Grid container direction="column">
                     <Link target="_blank"
                       href="https://robertpengcode.github.io/1945-OKINAWA/" >
                         <Grid item className={classes.imgBoxOkinawa}></Grid>
                     </Link>
                   <Grid item>
-                    <Typography className={classes.text}>text</Typography>
+                    <Typography className={classes.text}>A shooting game using JavaScript, HTML, CSS, Bootstrap. The background is the battle of Okinawa happened in 1945.</Typography>
                   </Grid>
                 </Grid>
+                </Paper>
               </Grid>
             </Grid>
           </Grid>
